a328f3142dc630bae0c90cfa1db2cc215b80686e
GlabLukasz <glab_lukasz@o2.pl>
2022-10-07 14:29:39 +0200

    Modified taks to run for trening purposes

--- python_tutorial/rng.py
+++ python_tutorial/rng.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python3
+from random import randint
 
-from random import randit
+min_number=int(input('Please insert minimal number: '))
+max_number=int(input('Please insert maximal number: '))
 
-min_number=init(input('Please insert minimal number: '))
-max_number=init(input('Please insert maximal number: '))
 
-def main():
-    if max_number < min_number:
-        print('Invalid input')
-    else:
-        rnd_number=randit(min_number, max_number)
-        print(rnd_number)
-
-if __name__ == "__main__":
-    main()
+if max_number < min_number:
+    print('Invalid input')
+else:
+    rnd_number=randint(min_number, max_number)
+    print(rnd_number)
